f6ae3667cfc58d676ed5f4e55424dfb3891a2235
clewis2211 <carltonlewis42@gmail.com>
2022-04-13 13:41:09 -0500

    upgraded the form layout and header

---
+++ scripts.js
@@ -0,0 +1,51 @@
+// let addBtn = document.getElementById('addBtn');
+// addBtn.addEventListener('submit', addItem);
+
+let form = document.getElementById('form');
+let todoItems = document.getElementById('todoItems');
+todoItems.addEventListener('click', removeItem)
+
+form.addEventListener('submit', addItem)
+// form.addEventListener('click', clearInputBox)
+
+function addItem(e){
+    e.preventDefault()
+    // console.log(e.target)
+
+    //GET INPUT VALUE
+    let newItem = document.getElementById('inputItem').value
+    console.log(newItem)
+    let li = document.createElement('li');
+    li.className = 'listGroup'
+    let text = document.createTextNode(newItem)
+
+    li.appendChild(text);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className = "btn btn-primary btn-sm float-right delete";
+    deleteBtn.id = 'deleteBtn';
+    deleteBtn.style.float = "right";
+    deleteBtn.style.marginBottom = '10px'
+    let deleteIcon = document.createTextNode('X')
+    deleteBtn.appendChild(deleteIcon)
+
+    document.getElementById('inputItem').value = '';
+
+    let checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    newItem.appendChild(checkBox);
+
+    li.appendChild(deleteBtn)
+    todoItems.appendChild(li)
+}
+
+function removeItem(e){
+    //check to see if what we are clicking has the class delete in it
+    if(e.target.classList.contains('delete')){
+        let li = e.target.parentElement;
+        todoItems.removeChild(li)
+    }
+}
+
+function clearInputBox(e){
+}
